238c70efa8039138c180db06aa0d550a95b32e1c
Chuks1993 <chuksgrinage.dev@gmail.com>
2022-03-14 20:28:26 -0500

    remove async

--- apps/web/src/components/add-reminder-form/index.test.tsx
+++ apps/web/src/components/add-reminder-form/index.test.tsx
@@ -4,10 +4,8 @@ import { AddReminderForm } from '.'
 describe('AddReminderForm', () => {
   render(<AddReminderForm />)
   test('should render button', async () => {
-    await waitFor(() =>
-      expect(
-        screen.getByRole('button', { name: /add reminder/i })
-      ).toBeInTheDocument()
-    )
+    expect(
+      screen.getByRole('button', { name: /add reminder/i })
+    ).toBeInTheDocument()
   })
 })
