48c6c14ebd6425c7e3243f30740586fd454bdd3b
Chuks1993 <chuksgrinage.dev@gmail.com>
2022-04-25 00:53:52 -0500

    update

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -4,7 +4,7 @@ export const gqlClient =
     variables?: TVariables
   ): (() => Promise<TData>) =>
   async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
       method: 'POST',
       credentials: 'include',
       headers: {
